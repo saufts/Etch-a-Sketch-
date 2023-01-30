5f59c4c176d311a7d5188c044c1b82c04fa6be05
saufts <l.szwaczka90@gmail.com>
2023-01-30 21:34:23 +0100

    Finish deleteGrid function, invoked when button is clicked for at least second time

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const container = document.querySelector('.container');
 
-createGrid(144);
+let rows = [];
+
+changeGridSizeButton = document.querySelector('button');
+changeGridSizeButton.addEventListener('click', () => {
+    if(isGridCreated) deleteGrid();
+    
+    gridSize = prompt('Enter grid size');
+
+
+    createGrid(gridSize);
+})
+
+let isGridCreated = false;
 
 function createGrid(gridCount) {
 
     for (let i = 0; i < gridCount; i++) {
 
-        rows = document.createElement('div');
+
+        rows[i] = document.createElement('div');
     
-        rows.classList.add(`rows`);
-        container.appendChild(rows);
+        rows[i].classList.add(`rows`);
+        container.appendChild(rows[i]);
         
         for (let j = 0; j < gridCount; j++) {
 
             let columns = [];
             columns[j] = document.createElement('div');
             columns[j].classList.add(`columns`);
-            rows.appendChild(columns[j]);
+            rows[i].appendChild(columns[j]);
             //columns[j].textContent = j + 1;  
         }       
     }
@@ -26,5 +39,13 @@ function createGrid(gridCount) {
             column.style.background = 'red';
         });
     });
+    isGridCreated = true;
 }
 
+function deleteGrid() {
+
+    for (let i = 0; i < gridSize; i++) {
+    
+        rows[i].parentNode.removeChild(rows[i]);
+    }       
+} 
